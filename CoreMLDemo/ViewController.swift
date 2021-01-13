e193e1265398d22db0cf0e19e4e9a9e07331cc98
Sambit Das <sambit.das@zopsmart.com>
2021-01-14 02:02:17 +0530

    Initial Commit

---
+++ CoreMLDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreMLDemo
+//
+//  Created by Sambit Das on 14/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
